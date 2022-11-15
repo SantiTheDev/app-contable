6a0295bd1f4afaf804fe19fe500eb5210ec5a070
santiago <santiago@ojedasodev.ojedasodev.com>
2022-11-15 00:12:14 -0500

    Update pipeline file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,27 @@
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "DevOps"
+    storage_account_name = "tearraformstatefiles"
+    container_name       = "tfstate"
+    key                  = "remote.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
 resource "azurerm_resource_group" "DevOps_rg" {
-  name = "DevOps"
+  name = "DevOps-container"
   location = "eastus"
 }
 
